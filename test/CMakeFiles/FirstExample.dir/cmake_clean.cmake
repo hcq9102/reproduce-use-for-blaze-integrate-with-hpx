8b2ce1a9c8388dba8488ffba96460b89eb2bf811
Chuanqiu He <49005493+hcq9102@users.noreply.github.com>
2022-08-04 15:20:03 -0500

    Add files via upload

---
+++ test/CMakeFiles/FirstExample.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FirstExample.dir/FirstExample.cpp.o"
+  "CMakeFiles/FirstExample.dir/FirstExample.cpp.o.d"
+  "FirstExample"
+  "FirstExample.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FirstExample.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
